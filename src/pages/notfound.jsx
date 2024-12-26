3272fe7110795e4cb2356bc3f147e8de8b826b92
Bhushancodetech <codetentaclesui38@gmail.com>
2024-12-26 16:17:13 +0530

    updated

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -9,7 +9,7 @@ function notfound() {
       <div className="flex flex-col items-center justify-center">
         <img src={NotFound} />
         <h1 className='font-extrabold text-5xl text-center py-4'>Page Not Found</h1>
-        <p className='text-sm'>Hey User! Looks like you’re heading to a wrong page</p>
+        <p className='text-md text-[#575757]'>Hey User! Looks like you’re heading to a wrong page</p>
         <button className='bg-[#383838] rounded-[8px] text-white my-5 py-2 px-5 text-xl flex items-center gap-2'>  <ArrowLeft size={24} />  Go Back</button>
       </div>
     </>
