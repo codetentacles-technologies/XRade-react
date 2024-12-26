dedc14a8f0fc11005e4210d293393f84f120e7c4
Bhushancodetech <codetentaclesui38@gmail.com>
2024-12-26 16:35:19 +0530

    updated

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NotFound from "../../public/images/404.svg"
 import { ArrowLeft } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 
 function notfound() {
@@ -8,9 +9,9 @@ function notfound() {
     <>
       <div className="flex flex-col items-center justify-center">
         <img src={NotFound} />
-        <h1 className='font-extrabold text-5xl text-center py-4'>Page Not Found</h1>
-        <p className='text-md text-[#575757]'>Hey User! Looks like you’re heading to a wrong page</p>
-        <button className='bg-[#383838] rounded-[8px] text-white my-5 py-2 px-5 text-xl flex items-center gap-2'>  <ArrowLeft size={24} />  Go Back</button>
+        <h1 className='font-extrabold text-5xl text-center py-4 text-blue'>Page Not Found</h1>
+        <p className='text-primary'>Hey User! Looks like you’re heading to a wrong page</p>
+       <Link to="/"> <button className='bg-blue rounded-[8px] text-white my-5 py-2 px-5 text-xl flex items-center gap-2 hover:bg-primary transform transition-all group duration-300 ease-in-out'>  <ArrowLeft size={24} />  Go Back</button></Link>
       </div>
     </>
   )
