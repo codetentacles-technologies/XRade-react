4e7fa635f90f2a80c35d1daf997d472c29bf310c
Jeevika Huske <jeevikacodetech@gmail.com>
2025-01-08 13:12:05 +0530

    changes

--- src/pages/Voucher.jsx
+++ src/pages/Voucher.jsx
@@ -21,10 +21,10 @@ const [isPopupOpen, setIsPopupOpen] = useState(false);
     return (
         <>
         {isPopupOpen && <WithdrawPopup handleClose={handleClosePopup} />}
-        <div className='container text-primary overflow-auto h-auto'>
+        <div className=' text-primary '>
             <BreadCrumbs title="Voucher" breadcrumbs={breadcrumbs} />
             <div className='flex flex-col items-center justify-center h-page'>
-                <div className='flex flex-col items-center justify-center mx-auto gap-[74px] h-min bg-bluegradient text-white p-[32px] rounded-2xl w-full md:w-2/3 lg:w-1/2'>
+                <div className='flex flex-col items-center justify-center mx-auto gap-[74px] h-min bg-bluegradient text-white p-[32px] rounded-2xl w-full md:w-2/3 lg:w-1/2 xl:w-[40%]'>
                     <div className='flex flex-col gap-8 items-center justify-center'>
                         <h2 className='text-2xl md:text-4xl font-bold'>Congratulations !</h2>
                         <p className='text-base font-normal text-center'>This dashboard is your gateway to real-time insights, empowering you to track and maximize your earnings effortlessly. Here, you'll find up-to-the-minute updates on your investments, referral performance, and rewards.</p>
