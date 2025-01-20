e0ef4b0acea38190abc5d1ee2faa8299690b44c3
saisurvase <48916216+saisurvase@users.noreply.github.com>
2025-01-20 20:15:47 +0530

    form variable changed

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -213,7 +213,7 @@ const Admin = () => {
                     <label className="block text-sm font-medium text-gray-600 mb-2">Amount</label>
                     <input
                         type="text"
-                        value={form1Field1}
+                        value={form1Field2}
                         onChange={(e) => setForm1Field2(e.target.value)}
                         required
                         className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
